a81a6bfb6483d39acd227e52e4ef1b0f8d3f8d77
sbom00 <lnakatani@yahoo.com.br>
2020-03-19 10:56:38 -0300

    order_items_schema

---
+++ database/migrations/1584625989840_order_item_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class OrderItemSchema extends Schema {
+  up () {
+    this.create('order_items', (table) => {
+      table.increments()
+      table.integer('product_id').usigned()
+      table.integer('quantity').usigned
+      table.decimal('subtotal',12,2)
+      table.integer('order_id').usigned()
+      table.foreign('product_id').references('id').inTable('products').onDelete('cascade')
+      table.foreign('order_id').references('id').inTable('orders').onDelete('cascade')
+    })
+  }
+
+  down () {
+    this.drop('order_items')
+  }
+}
+
+module.exports = OrderItemSchema
